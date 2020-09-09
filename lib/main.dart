ea3873aa01a8c077159959cce0d8a49ce7ea5683
heathkev <kevintuazon45@gmail.com>
2020-09-09 11:47:22 +0800

    Added on pressed function

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.red,
       appBar: AppBar(
         title: Text('Dice'),
         backgroundColor: Colors.red,
@@ -16,15 +16,31 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image(image: AssetImage('images/dice1.png')),
-        ),
-        Expanded(
-          child: Image(image: AssetImage('images/dice2.png')),
-        )
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Left button got pressed.');
+              },
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Right button got pressed');
+              },
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
